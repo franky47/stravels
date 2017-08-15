33db188efc200274a8db12f18e886d67df486939
Francois Best <francoisbest@gmail.com>
2017-08-16 00:41:04 +0200

    Working !

--- App/Containers/ActivityScreen.js
+++ App/Containers/ActivityScreen.js
@@ -1,48 +1,35 @@
 import React, { Component } from 'react'
-import { View, Text, ListView } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import { connect } from 'react-redux'
 import Api from '../Services/FixtureApi'
 import ActivityRow from '../Components/ActivityRow'
-
-// For empty lists
-// import AlertMessage from '../Components/AlertMessage'
 
 // Styles
 import styles from './Styles/ActivityScreenStyle'
 
 class ActivityScreen extends Component {
-  state: {
-    dataSource: Object
-  }
 
   constructor (props) {
     super(props)
-    /* ***********************************************************
-    * STEP 1
-    * This is an array of objects with the properties you desire
-    * Usually this should come from Redux mapStateToProps
-    *************************************************************/
 
-    /* ***********************************************************
-    * STEP 2
-    * Teach datasource how to detect if rows are different
-    * Make this function fast!  Perhaps something like:
-    *   (r1, r2) => r1.id !== r2.id}
-    *************************************************************/
-    const rowHasChanged = (r1, r2) => r1 !== r2
-
-    // DataSource configured
-    const ds = new ListView.DataSource({rowHasChanged})
-
-    // Datasource is always in state
     this.state = {
-      dataSource: ds.cloneWithRows(Api.getActivities()),
-      selectable: false
+      data: Api.getActivities(),
+      selectable: false,
+      selected: []
     }
   }
 
   onRowPress (id) {
-
+    const index = this.state.selected.indexOf(id)
+    const select = (selected) => [...selected, id]
+    const deselect = (selected) => {
+      const copy = [...selected]
+      copy.splice(index, 1)
+      return copy
+    }
+    this.setState((state, props) => ({
+      selected: (index > -1) ? deselect(state.selected) : select(state.selected)
+    }))
   }
   onRowLongPress (id) {
     this.setState({
@@ -50,76 +37,29 @@ class ActivityScreen extends Component {
     })
   }
 
-  /* ***********************************************************
-  * STEP 3
-  * `renderRow` function -How each cell/row should be rendered
-  * It's our best practice to place a single component here:
-  *
-  * e.g.
-    return <MyCustomCell title={rowData.title} description={rowData.description} />
-  *************************************************************/
-  renderRow (activity) {
-    const selectable = this.state.selectable
-    return (
-      <ActivityRow
-        title={activity.name}
-        type={activity.type}
-        distance={activity.distance}
-        elevation={activity.total_elevation_gain}
-        imageUri={'https://fse-avatars.surge.sh/fse.png'}
-        checked={activity.distance > 50000}
-        selectable={selectable}
-        onPress={this.onRowPress.bind(this, activity.id)}
-        onLongPress={this.onRowLongPress.bind(this, activity.id)}
-      />
-    )
-  }
-
-  /* ***********************************************************
-  * STEP 4
-  * If your datasource is driven by Redux, you'll need to
-  * reset it when new data arrives.
-  * DO NOT! place `cloneWithRows` inside of render, since render
-  * is called very often, and should remain fast!  Just replace
-  * state's datasource on newProps.
-  *
-  * e.g.
-    componentWillReceiveProps (newProps) {
-      if (newProps.someData) {
-        this.setState(prevState => ({
-          dataSource: prevState.dataSource.cloneWithRows(newProps.someData)
-        }))
-      }
-    }
-  *************************************************************/
-
-  // Used for friendly AlertMessage
-  // returns true if the dataSource is empty
-  noRowData () {
-    return this.state.dataSource.getRowCount() === 0
-  }
-
-  // Render a footer.
-  renderFooter = () => {
-    return (
-      <Text> - Footer - </Text>
-    )
+  renderItem = ({item}) => {
+    const selected = this.state.selected.indexOf(item.id) > -1
+    return <ActivityRow
+      title={item.name}
+      type={item.type}
+      distance={item.distance}
+      elevation={item.total_elevation_gain}
+      imageUri={'https://fse-avatars.surge.sh/fse.png'}
+      checked={selected}
+      selectable={this.state.selectable}
+      onPress={this.onRowPress.bind(this, item.id)}
+      onLongPress={this.onRowLongPress.bind(this, item.id)}
+    />
   }
 
   render () {
-    return (
-      <View style={styles.container}>
-        <ListView
-          contentContainerStyle={styles.listContent}
-          dataSource={this.state.dataSource}
-          renderRow={this.renderRow.bind(this)}
-          renderFooter={this.renderFooter}
-          enableEmptySections
-          pageSize={10}
-          extraData={this.state}
-        />
-      </View>
-    )
+    return <FlatList
+      style={styles.container}
+      data={this.state.data}
+      extraData={this.state}
+      renderItem={this.renderItem.bind(this)}
+      keyExtractor={(item) => item.id}
+    />
   }
 }
 
--- App/Containers/Styles/ActivityScreenStyle.js
+++ App/Containers/Styles/ActivityScreenStyle.js
@@ -27,45 +27,4 @@ export default StyleSheet.create({
   listContent: {
     marginTop: Metrics.baseMargin
   },
-  activityRow: {
-    flex: 1,
-    flexDirection: 'row',
-    paddingTop: Metrics.smallMargin,
-    paddingBottom: Metrics.smallMargin,
-
-    // Border
-    borderBottomWidth: 1,
-    borderStyle: 'solid',
-    borderColor: Colors.separator,
-    borderRadius: Metrics.buttonRadius
-  },
-  activityIcon: {
-    width: Metrics.icons.large,
-    height: Metrics.icons.large,
-    marginLeft: Metrics.baseMargin,
-    marginRight: Metrics.baseMargin,
-    borderRadius: Metrics.buttonRadius
-  },
-  activityTextContainer: {
-    flex: 1,
-    flexDirection: 'column'
-  },
-  activityTitle: {
-    fontWeight: 'bold'
-  },
-  activityDescriptionContainer: {
-    flex: 1,
-    marginTop: Metrics.smallMargin,
-    flexDirection: 'row'
-  },
-  activityDescriptionIcon: {
-    marginRight: Metrics.smallMargin,
-    color: Colors.secondary,
-    fontSize: 14
-  },
-  activityDescriptionText: {
-    marginRight: Metrics.baseMargin,
-    color: Colors.secondary,
-    fontSize: Fonts.size.small
-  }
 })
