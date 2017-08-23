376fd038d664d2479193416bbf17b4a351bc2710
Francois Best <francoisbest@gmail.com>
2017-08-23 09:56:39 +0200

    Added event listener helper.

---
+++ App/Utility/EventEmitter.js
@@ -0,0 +1,30 @@
+
+export default class EventEmitter {
+  constructor() {
+    this.listeners = new Set()
+  }
+
+  addListener(callback) {
+    this.listeners.add(callback)
+    return () => {
+      this.listeners.delete(callback)
+    }
+  }
+  addListeners(callbacks) {
+    callbacks.forEach((callback) => {
+      this.addListener(callback)
+    })
+  }
+  removeAllListeners() {
+    this.listeners.clear()
+  }
+  notifyListenersSync(...args) {
+    this.listeners.forEach(callback => {
+      callback(...args)
+    })
+  }
+  notifyListeners(...args) {
+    const promises = [...this.listeners].map((callback) => Promise.resolve(callback(...args)))
+    return Promise.all(promises)
+  }
+}
---
+++ Tests/Utility/EventEmitterTest.js
@@ -0,0 +1,127 @@
+import EventEmitter from '../../App/Utility/EventEmitter'
+
+describe('Listener Management', () => {
+  test('addListener', () => {
+    const emitter = new EventEmitter()
+    const listener = () => {}
+    const unsubscribe = emitter.addListener(listener)
+    expect(unsubscribe).toBeInstanceOf(Function)
+    expect(emitter.listeners.size).toEqual(1)
+  })
+  test('removeListener', () => {
+    const emitter = new EventEmitter()
+    const listener = () => {}
+    const unsubscribe = emitter.addListener(listener)
+    unsubscribe()
+    expect(emitter.listeners.size).toEqual(0)
+  })
+  test('add duplicate listener', () => {
+    const emitter = new EventEmitter()
+    const listener = () => {}
+    const unsubscribe1 = emitter.addListener(listener)
+    const unsubscribe2 = emitter.addListener(listener)
+    expect(unsubscribe1).toBeInstanceOf(Function)
+    expect(unsubscribe2).toBeInstanceOf(Function)
+    expect(emitter.listeners.size).toEqual(1)
+  })
+  test('remove duplicate listener, unsub 1', () => {
+    const emitter = new EventEmitter()
+    const listener = () => {}
+    const unsubscribe1 = emitter.addListener(listener)
+    const unsubscribe2 = emitter.addListener(listener)
+    unsubscribe1()
+    expect(emitter.listeners.size).toEqual(0)
+  })
+  test('remove duplicate listener, unsub 2', () => {
+    const emitter = new EventEmitter()
+    const listener = () => {}
+    const unsubscribe1 = emitter.addListener(listener)
+    const unsubscribe2 = emitter.addListener(listener)
+    unsubscribe2()
+    expect(emitter.listeners.size).toEqual(0)
+  })
+  test('remove duplicate listener, unsub both', () => {
+    const emitter = new EventEmitter()
+    const listener = () => {}
+    const unsubscribe1 = emitter.addListener(listener)
+    const unsubscribe2 = emitter.addListener(listener)
+    unsubscribe1()
+    unsubscribe2()
+    expect(emitter.listeners.size).toEqual(0)
+  })
+})
+
+describe('Notifications (sync)', () => {
+  test('notifySync, no args', () => {
+    const emitter = new EventEmitter()
+    const foo = jest.fn()
+    const bar = jest.fn()
+    emitter.addListeners([foo, bar])
+    expect(emitter.listeners.size).toEqual(2)
+    emitter.notifyListenersSync()
+    expect(foo).toHaveBeenCalledWith()
+    expect(bar).toHaveBeenCalledWith()
+    // Removed listeners should not be called again
+    foo.mockReset()
+    bar.mockReset()
+    emitter.removeAllListeners()
+    emitter.notifyListenersSync()
+    expect(foo).not.toHaveBeenCalledWith()
+    expect(bar).not.toHaveBeenCalledWith()
+  })
+  test('notifySync, 1 arg', () => {
+    const emitter = new EventEmitter()
+    const foo = jest.fn()
+    const bar = jest.fn()
+    emitter.addListeners([foo, bar])
+    emitter.notifyListenersSync('hello')
+    expect(foo).toHaveBeenCalledWith('hello')
+    expect(bar).toHaveBeenCalledWith('hello')
+  })
+  test('notifySync, 2 arg', () => {
+    const emitter = new EventEmitter()
+    const foo = jest.fn()
+    const bar = jest.fn()
+    emitter.addListeners([foo, bar])
+    emitter.notifyListenersSync('hello', { world: '!' })
+    expect(foo).toHaveBeenCalledWith('hello', { world: '!' })
+    expect(bar).toHaveBeenCalledWith('hello', { world: '!' })
+  })
+})
+
+describe('Notifications (async)', () => {
+  test('notifyAsync, no args', async () => {
+    const emitter = new EventEmitter()
+    const foo = jest.fn().mockImplementation((...args) => 'foo')
+    const bar = jest.fn().mockImplementation((...args) => 'bar')
+    emitter.addListeners([foo, bar])
+
+    const promise = await emitter.notifyListeners()
+    expect(promise).toEqual(['foo', 'bar'])
+    expect(foo).toHaveBeenCalledWith()
+    expect(bar).toHaveBeenCalledWith()
+  })
+  test('notifySync, 1 arg', async () => {
+    const emitter = new EventEmitter()
+    const foo = jest.fn().mockImplementation((...args) => 'foo')
+    const bar = jest.fn().mockImplementation((...args) => 'bar')
+    emitter.addListeners([foo, bar])
+
+    const promise = await emitter.notifyListeners('hello')
+    expect(promise).toEqual(['foo', 'bar'])
+    expect(foo).toHaveBeenCalledWith('hello')
+    expect(bar).toHaveBeenCalledWith('hello')
+  })
+  test('notifySync, 2 arg', async () => {
+    const emitter = new EventEmitter()
+    const foo = jest.fn().mockImplementation((...args) => 'foo')
+    const bar = jest.fn().mockImplementation((...args) => 'bar')
+    emitter.addListeners([foo, bar])
+
+    const promise = await emitter.notifyListeners('hello', { world: '!' })
+    expect(promise).toEqual(['foo', 'bar'])
+    expect(foo).toHaveBeenCalledWith('hello', { world: '!' })
+    expect(bar).toHaveBeenCalledWith('hello', { world: '!' })
+  })
+})
+
