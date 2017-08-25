ba7db7191df97ed2798b844aeb0432d592a3a157
Francois Best <francoisbest@gmail.com>
2017-08-25 09:14:24 +0200

    Added test MapView

---
+++ App/Components/MapView.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types';
+import { View, Text } from 'react-native'
+import styles from './Styles/MapViewStyle'
+
+import Mapbox, { MapView as Map } from 'react-native-mapbox-gl'
+import Secrets from 'react-native-config'
+
+Mapbox.setAccessToken(Secrets.MAPBOX_ACCESS_TOKEN)
+
+export default class MapView extends Component {
+  // // Prop type warnings
+  // static propTypes = {
+  //   someProperty: PropTypes.object,
+  //   someSetting: PropTypes.bool.isRequired,
+  // }
+  //
+  // // Defaults for props
+  // static defaultProps = {
+  //   someSetting: false
+  // }
+
+  static navigationOptions = {
+    title: 'Map'
+  };
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      center: {
+        latitude: 45.1836282,
+        longitude: 5.7431109
+      },
+      zoom: 10
+    }
+  }
+
+  configure (map) {
+    this.map = map
+    console.tron.log('Map configured.')
+  }
+
+  render () {
+    return (
+      <Map
+        style={styles.container}
+        initialCenterCoordinate={this.state.center}
+        initialZoomLevel={this.state.zoom}
+        ref={map => this.configure(map)}
+        styleURL={'mapbox://styles/mapbox/outdoors-v10'}
+      />
+    )
+  }
+}
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,20 +1,20 @@
 import { StackNavigator } from 'react-navigation'
 import StravaLoginScreen from '../Containers/StravaLoginScreen'
-import LoginScreen from '../Containers/LoginScreen'
 import ActivityScreen from '../Containers/ActivityScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
+import MapView from '../Components/MapView'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
   StravaLoginScreen: { screen: StravaLoginScreen },
-  LoginScreen: { screen: LoginScreen },
   ActivityScreen: { screen: ActivityScreen },
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: LaunchScreen },
+  MapView: { screen: MapView }
 }, {
   // Default config for all screens
-  headerMode: 'none',
+  // headerMode: 'none',
   initialRouteName: 'StravaLoginScreen',
   navigationOptions: {
     headerStyle: styles.header
