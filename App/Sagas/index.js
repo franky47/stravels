ef118dd6247f7d10f07b41dea4b34f5e5480b1d8
Francois Best <francoisbest@gmail.com>
2017-08-27 21:37:59 +0200

    Cleanup.

--- App/Sagas/index.js
+++ App/Sagas/index.js
@@ -1,20 +1,4 @@
-// import { takeLatest } from 'redux-saga/effects'
-// import API from '../Services/Api'
-// import FixtureAPI from '../Services/FixtureApi'
-// import DebugConfig from '../Config/DebugConfig'
-
-/* ------------- Types ------------- */
-
-// import { StartupTypes } from '../Redux/StartupRedux'
-// import { GithubTypes } from '../Redux/GithubRedux'
-
-/* ------------- Sagas ------------- */
-
-// import { startup } from './StartupSagas'
-// import { getUserAvatar } from './GithubSagas'
 import stravaSaga from './StravaSagas'
-
-/* ------------- Connect Types To Sagas ------------- */
 
 export default function * root () {
   yield [
