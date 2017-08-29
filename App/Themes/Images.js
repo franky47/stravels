091d4581eff7ad490db70c18c79566868603be6e
Francois Best <francoisbest@gmail.com>
2017-08-29 21:59:01 +0200

    Added images.

--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -1,5 +1,20 @@
 // leave off @2x/@3x
 const images = {
+
+  loginLogo: require('../Images/login-logo.png'),
+
+  strava: {
+    connect: {
+      orange: require('../Images/strava/connect-orange.png'),
+      light: require('../Images/strava/connect-light.png')
+    },
+    poweredBy: {
+      light: require('../Images/strava/powered-light.png'),
+      white: require('../Images/strava/powered-white.png')
+    }
+  },
+
+  // Ignite stuff
   logo: require('../Images/ignite/ir.png'),
   clearLogo: require('../Images/ignite/top_logo.png'),
   launch: require('../Images/ignite/launch-icon.png'),
