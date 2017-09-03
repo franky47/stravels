8c0e83c4a4e6335948ca53150c86cf2942afc8ad
Francois Best <francoisbest@gmail.com>
2017-09-03 17:42:44 +0200

    Added service to build Mapbox Static urls.

---
+++ src/services/mapboxStatic.js
@@ -0,0 +1,23 @@
+import Secrets from 'react-native-config'
+
+const defaultOptions = {
+  width: 100,
+  height: 100,
+  retina: true,
+  format: 'png',
+  mapId: 'mapbox.streets',
+  strokeWidth: 4,
+  strokeColor: 'f00',
+  strokeOpacity: 1.0,
+  fillColor: '000',
+  fillOpacity: 0.0
+}
+
+export const getPolylineUrl = (polyline, options = defaultOptions) => {
+  const opt = {...defaultOptions, ...options}
+  const baseUrl = `https://api.mapbox.com/v4`
+  const overlay = `path-${opt.strokeWidth}+${opt.strokeColor}-${opt.strokeOpacity}+${opt.fillColor}-${opt.fillOpacity}(${encodeURIComponent(polyline)})`
+  const retina = opt.retina ? '@2x' : ''
+  const token = Secrets.MAPBOX_ACCESS_TOKEN
+  return `${baseUrl}/${opt.mapId}/${overlay}/auto/${opt.width}x${opt.height}${retina}.${opt.format}?access_token=${token}`
+}
