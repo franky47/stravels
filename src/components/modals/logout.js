333271d28d499aa6a85f5c141a186fbf3ca2d425
Francois Best <francoisbest@gmail.com>
2017-09-04 13:34:06 +0200

    Added logout alert.

---
+++ src/components/modals/logout.js
@@ -0,0 +1,18 @@
+import { Alert } from 'react-native'
+
+export default (onConfirm, onDismiss = () => {}) => {
+  const buttons = [
+    {
+      text: 'Cancel',
+      onPress: onDismiss,
+      style: 'cancel'
+    },
+    {
+      text: 'Log Out',
+      onPress: onConfirm,
+      style: 'destructive'
+    }
+  ]
+  const options = { onDismiss } // Andoid can dismiss by tapping outside.
+  Alert.alert('Log out', 'Are you sure ?', buttons, options)
+}
