47dd9a18b5f6e8b37a6a17a38a48e2dae3da8f76
Francois Best <francoisbest@gmail.com>
2017-09-06 14:10:24 +0200

    Added isLoggedIn selector.

--- src/redux/strava/oauth/selectors.js
+++ src/redux/strava/oauth/selectors.js
@@ -7,5 +7,12 @@ export default (stateMapper = (s) => s) => ({
   getPhase: (parentState) => getPhase(stateMapper(parentState)),
   isFetching: (parentState) => isFetching(stateMapper(parentState)),
   getToken: (parentState) => getToken(stateMapper(parentState)),
-  getError: (parentState) => getError(stateMapper(parentState))
+  getError: (parentState) => getError(stateMapper(parentState)),
+
+  // Computed
+  isLoggedIn: (parentState) => {
+    const phase = getPhase(stateMapper(parentState))
+    const token = getToken(stateMapper(parentState))
+    return phase === 'LOGGED_IN' && token !== null
+  }
 })
