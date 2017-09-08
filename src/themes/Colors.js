4e06dad40857b1080c913bc9e8b976f4c016801b
Francois Best <francoisbest@gmail.com>
2017-09-08 09:23:11 +0200

    Use different main colors on iOS and Android.

--- src/themes/Colors.js
+++ src/themes/Colors.js
@@ -1,12 +1,17 @@
+import { Platform } from 'react-native'
+
 const colors = {
   background: '#ffffff',
-  clear: 'rgba(0,0,0,0)',
+  clear: 'rgba(0, 0, 0, 0)',
+  transparent: 'rgba(0, 0, 0, 0)',
   facebook: '#3b5998',
-  transparent: 'rgba(0,0,0,0)',
   strava: '#fc4C02',
 
-  main: '#47cc5e',
-  text: 'black', // '#9C27B0'
+  main: Platform.select({
+    android: '#4CAF50', // Material green 500
+    ios: '#47cc5e'      // Apple green
+  }),
+  text: 'black',
 
   separator: 'rgba(0, 0, 0, 0.1)',
   secondary: 'grey',
