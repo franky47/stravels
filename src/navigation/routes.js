8523b874b14301e056f02a4eb58bceb090886fc8
Francois Best <francoisbest@gmail.com>
2017-09-08 14:35:03 +0200

    Renamed screen.

--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -24,7 +24,7 @@ export default {
   Travel: {
     screen: screens.TravelScreen,
     navigationOptions: {
-      title: 'Travel Summary',
+      title: 'Summary',
       ...header.main.navigationOptions
     }
   },
