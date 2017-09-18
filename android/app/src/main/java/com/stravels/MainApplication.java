c52e3e5ccc927581e5bf0dd960ad519e088942c4
Francois Best <francoisbest@gmail.com>
2017-09-18 22:46:00 +0200

    chore: upgrade dependencies
    Requires to patch tests/setup.js, see facebook/react-native#15810

--- android/app/src/main/java/com/stravels/MainApplication.java
+++ android/app/src/main/java/com/stravels/MainApplication.java
@@ -10,7 +10,7 @@ import com.microsoft.azure.mobile.react.analytics.RNAnalyticsPackage;
 import com.microsoft.azure.mobile.react.mobilecenter.RNMobileCenterPackage;
 import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
@@ -39,16 +39,16 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG),
-            new RNPushPackage(MainApplication.this),
-            new RNCrashesPackage(MainApplication.this, getResources().getString(R.string.mobileCenterCrashes_whenToSendCrashes)),
-            new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics)),
-            new RNMobileCenterPackage(MainApplication.this),
-            new ReactNativeMapboxGLPackage(),
-            new ReactNativeConfigPackage(),
-            new ReactNativeI18n(),
-            new VectorIconsPackage(),
-            new RNDeviceInfo()
+          new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG),
+          new RNPushPackage(MainApplication.this),
+          new RNCrashesPackage(MainApplication.this, getResources().getString(R.string.mobileCenterCrashes_whenToSendCrashes)),
+          new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics)),
+          new RNMobileCenterPackage(MainApplication.this),
+          new ReactNativeMapboxGLPackage(),
+          new ReactNativeConfigPackage(),
+          new RNI18nPackage(),
+          new VectorIconsPackage(),
+          new RNDeviceInfo()
       );
     }
   };
