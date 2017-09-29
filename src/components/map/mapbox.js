931972189c016adcf0b6998b526036f7a5da5584
Francois Best <francoisbest@gmail.com>
2017-09-29 22:03:40 +0200

    fix: printing secrets to console, really ?

--- src/components/map/mapbox.js
+++ src/components/map/mapbox.js
@@ -1,7 +1,6 @@
 import Mapbox, { MapView } from 'react-native-mapbox-gl'
 import Secrets from 'react-native-config'
 
-console.log(Secrets)
 Mapbox.setAccessToken(Secrets.MAPBOX_ACCESS_TOKEN)
 
 export default MapView
