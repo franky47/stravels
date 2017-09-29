c4bf89d3913c05fb4781b7b2bb8c2a01b38161d5
Francois Best <francoisbest@gmail.com>
2017-09-30 00:06:40 +0200

    revert: create store as instance member
    Reverts 0cbeef56, creates other problems.

--- src/index.js
+++ src/index.js
@@ -5,21 +5,12 @@ import RootContainer from './rootContainer'
 import createStore from './redux'
 import { wrapReactotronOverlay, wrapCodePush } from './utility/hof'
 
-/**
- * Provides an entry point into our application.  Both index.ios.js and index.android.js
- * call this component first.
- *
- * We create our Redux store here, put it into a provider and then bring in our
- * RootContainer.
- *
- * We separate like this to play nice with React Native's hot reloading.
- */
-class App extends Component {
-  store = createStore()
+const store = createStore()
 
+class App extends Component {
   render () {
     return (
-      <Provider store={this.store}>
+      <Provider store={store}>
         <RootContainer />
       </Provider>
     )
